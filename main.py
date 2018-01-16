8eea9103533d8fb1c178542b28a191b6d57a0ce2
Steffen Wolf <steffen.wolf@iwr.uni-heidelberg.de>
2018-01-16 21:05:18 +0100

    capture click data for later machine learning

--- main.py
+++ main.py
@@ -23,29 +23,33 @@ def draw_moves(position, moves, display):
     for end_pos in moves.values():
         pygame.draw.line(display, (0,255,0), tuple(position), tuple(end_pos), 2)
 
-def get_screenshot(fname='/home/swolf/local/src/zeno_mouse/data/screenshot_.png'):
+def get_screenshot(fname):
     # Uses Python Image Library
     import Image
     w = gtk.gdk.get_default_root_window()
     sz = w.get_size()
-    print "The size of the window is %d x %d" % sz
     pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
     pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
     pb.save(fname,"png")
     
 
-def load_shot(fname='/home/swolf/tmp/screenshot.png'):
+def load_shot(fname):
     surface = pygame.image.load(fname)
     surface.convert() # Makes blitting a bit faster
     return surface
 
+def save_data_point(fname, position):
+    with open("/home/swolf/local/src/zenos_mouse/data/labels.txt", "a") as label_file:
+        label_file.write("{},{},{}\n".format(fname, position[0], position[1]))
 
-def screenie():
+
+def main():
     # Set up PyGame
     pygame.init()
-    get_screenshot()
+    fname = "/home/swolf/local/src/zenos_mouse/data/screenshot_{}.png".format(time.time())
+    get_screenshot(fname)
     display = pygame.display.set_mode(pyautogui.size(), pygame.FULLSCREEN)
-    capture = load_shot()
+    capture = load_shot(fname)
     display.blit(capture, (0,0))
 
     position = np.array([p//2 for p in pyautogui.size()], dtype=int)
@@ -57,16 +61,22 @@ def screenie():
     key_to_gamekey = {"h":pygame.K_h, "j":pygame.K_j, "k":pygame.K_k, "l":pygame.K_l}
 
     while True:
-
         pygame.display.update()
         # Deal with QUIT events
         for e in pygame.event.get():
             if e.type == QUIT:
                 return
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                pygame.display.quit()
+                pyautogui.click(*position)
+                save_data_point(fname, pos)
+                return
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_f:
                     pygame.display.quit()
                     pyautogui.click(*position)
+                    save_data_point(fname, position)
                     return
                 if e.key == pygame.K_q:
                     pygame.display.quit()
@@ -86,4 +96,4 @@ def screenie():
 
 
 if __name__ == '__main__':
-    screenie()
+    main()
